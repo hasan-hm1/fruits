b76d967b6d1a6f08538e08105fb150692fd84b5e
Hasan Mohammed <hasanmohammed.hm1@gmail.com>
2024-03-31 22:36:32 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Hasan on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
